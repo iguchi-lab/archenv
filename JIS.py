51d2f9af5bdb957702d98fcf8e62a65bfdb78c57
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2025-02-10 12:42:43 +0900

    Create JIS.py

---
+++ JIS.py
@@ -0,0 +1,11 @@
+import archenv.archenv as ae
+
+JIS_T_C_IN = 27.0                                                             # JIS測定時の室内温度 [℃]
+JIS_T_C_EX = 35.0                                                             # JIS測定時の室外温度 [℃]
+JIS_X_C_IN = ae.absolute_humidity_from_e(ae.vapor_pressure_wet_bulb(19, 27))  # JIS測定時の室内絶対湿度 [kg/kg']  湿球温度は19℃
+JIS_X_C_EX = ae.absolute_humidity_from_e(ae.vapor_pressure_wet_bulb(24, 35))  # JIS測定時の室外絶対湿度 [kg/kg']  湿球温度は24℃
+
+JIS_T_H_IN = 20.0                                                             # JIS測定時の室内温度 [℃]
+JIS_T_H_EX =  7.0                                                             # JIS測定時の室外温度 [℃]
+JIS_X_H_IN = ae.absolute_humidity_from_e(ae.vapor_pressure_wet_bulb(15, 20))  # JIS測定時の室内絶対湿度 [kg/kg']  湿球温度は15℃（最高温度）
+JIS_X_H_EX = ae.absolute_humidity_from_e(ae.vapor_pressure_wet_bulb( 6,  7))  # JIS測定時の室外絶対湿度 [kg/kg']  湿球温度は6℃
