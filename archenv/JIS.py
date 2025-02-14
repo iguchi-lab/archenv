17777dc7e10f35762d080eadbc1c2c13ef0077ad
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2025-02-14 21:53:22 +0900

    Update JIS.py

--- archenv/JIS.py
+++ archenv/JIS.py
@@ -1,4 +1,4 @@
-import archenv.library as ae
+import archenv as ae
 
 T_C_IN = 27.0                                                             # JIS測定時の室内温度 [℃]
 T_C_EX = 35.0                                                             # JIS測定時の室外温度 [℃]
