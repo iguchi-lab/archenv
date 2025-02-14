60a478d688a856a325fbc9086e20a395f4854e45
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2025-02-14 13:42:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="archenv",  # 自分のパッケージ名
-    version="0.1.0",  # バージョン番号
-    packages=find_packages(),  # パッケージを自動検出
+    name="archenv",  # パッケージ名
+    version="0.1.0",  # バージョン
+    packages=find_packages(include=["archenv", "archenv.*"]),  # 明示的に archenv を含める
     install_requires=[
-        "requests",  # 依存関係のライブラリ
+        "requests",
         "numpy",
-    ],
-    dependency_links=[
+        "pandas"  # 必要なら追加
     ],
     include_package_data=True,
     zip_safe=False,
