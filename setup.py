59f3a2017b528aabc1ebf12e32751d2f195b2b83
IGUCHI Masato <67450288+iguchi-lab@users.noreply.github.com>
2025-02-14 22:39:16 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="archenv",  # パッケージ名
-    version="0.1.0",  # バージョン
-    packages=find_packages(include=["archenv", "archenv.*"]),  # 明示的に archenv を含める
+    name="ac_model",
+    version="0.1.0",
+    packages=find_packages(include=["ac_model", "ac_model.*"]),
     install_requires=[
         "requests",
         "numpy",
-        "pandas"  # 必要なら追加
+        "pandas",
+        "archenv",  # ここではパッケージ名のみ
+    ],
+    dependency_links=[
+        "git+https://github.com/iguchi-lab/archenv.git#egg=archenv",
     ],
     include_package_data=True,
-    zip_safe=False,
 )
